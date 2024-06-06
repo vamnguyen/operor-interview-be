2c285d9d3a20d631fc77fdb06b924c163f859aca
Minh Nguyen <anhminh.dev@gmail.com>
2024-06-06 21:30:23 +0700

    chore: Update server.ts to use process.env.PORT for dynamic port assignment

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import { app } from "./src";
-import { env } from "./src/utils/envConfig";
+// import { env } from "./src/utils/envConfig";
 import { prisma } from "./src/utils/prismaConfig";
 
-const server = app.listen(env.PORT, () => {
-  console.log(`Server is running on http://localhost:${env.PORT}`);
+const server = app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
 process.on("SIGINT", async () => {
