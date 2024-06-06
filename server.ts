bd9ff42a0ff9342baeb9416e80f4c47f39ae8bcb
Minh Nguyen <anhminh.dev@gmail.com>
2024-06-06 15:59:11 +0700

    test fix deploy

--- server.ts
+++ server.ts
@@ -1,9 +1,13 @@
 import { app } from "./src";
-import { env } from "./src/utils/envConfig";
+// import { env } from "./src/utils/envConfig";
 import { prisma } from "./src/utils/prismaConfig";
 
-const server = app.listen(env.PORT, () => {
-  console.log(`Server is running on http://localhost:${env.PORT}`);
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 process.on("SIGINT", async () => {
